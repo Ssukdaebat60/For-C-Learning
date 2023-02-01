10762facb28c8694c2e4a2fde4fe88ab2efbd325
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2023-02-02 08:46:18 +0900

    Create 02_cout&cin.cpp

---
+++ examples/02_cout&cin.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int val1;
+    std::cout<<"Enter the first number: ";
+    std::cin>>val1;
+    
+    int val2;
+    std::cout<<"Enter the second number: ";
+    std::cin>>val2;
+    
+    int result=val1+val2;
+    std::cout<<"result: "<<result<<std::endl;
+    return 0;
+}

912af4765d7ea03ea75cf87669cde7ca68984197
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2023-02-05 22:16:23 +0900

    Create CarMain.cpp

---
+++ examples/07_HeaderFile&Class&Preprocessor(#if)/CarMain.cpp
@@ -0,0 +1,11 @@
+#include "Car.h"
+
+int main() {
+    Car race99 = {2, 100};
+    race99.showCarState();
+    race99.Accel();
+    race99.showCarState();
+    race99.Accel();
+    race99.showCarState();
+    return 0;
+}

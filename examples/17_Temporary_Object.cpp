8246c4e7c3d093c9c21b95b526fb729325c30fa7
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2023-02-09 17:12:22 +0900

    Create 17_Temporary_Object.cpp

---
+++ examples/17_Temporary_Object.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+class tmpObj{
+    private:
+    public:
+        tmpObj(){
+        }
+        tmpObj(const tmpObj &copy) {
+            cout<<"copy constructor is called"<<endl;
+        }
+        tmpObj reVal(tmpObj ob){
+            cout<<"reVal_return : ";
+            return ob;
+        }
+        tmpObj* reRef(tmpObj ob){
+            cout<<"reRef_return : ";
+            return &ob;
+        }
+        void showInfo(){
+            cout<<this<<endl;
+        }
+};
+
+int main() {
+    tmpObj A;
+    tmpObj B(A);
+    A.showInfo();
+    B.showInfo();
+    cout<<endl;
+    A.reVal(B);
+    A.reRef(B);
+    return 0;
+}

d04ce5cb11f17301df67d4c651fe7e71fbdbaf4c
Ssukdaebat60 <84669895+Ssukdaebat60@users.noreply.github.com>
2023-02-13 09:09:13 +0900

    Create 25_Destructor_of_derived_obj.cpp

---
+++ examples/25_Destructor_of_derived_obj.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Person{
+    private:
+        char * name;
+    public:
+        Person(char * myname){
+            cout<<"Person()"<<endl;
+            name = new char[strlen(myname)+1];
+            strcpy(name, myname);
+        }
+        ~Person(){
+            cout<<"~Person()"<<endl;
+            delete []name;
+        }
+        void showPersonInfo() const{
+            cout<<"Name is "<<name<<endl;
+        }
+};
+class Student : public Person{
+    private:
+        char * major;
+    public:
+        Student(char * myname, char * mymajor) : Person(myname){
+            cout<<"Student()"<<endl;
+            major = new char[strlen(mymajor)];
+            strcpy(major, mymajor);
+        }
+        ~Student(){
+            cout<<"~Student()"<<endl;
+            delete []major;
+        }
+        void showStudentInfo() const{
+            showPersonInfo();
+            cout<<"Major is "<<major<<endl;
+        }
+};
+
+int main() {
+    Student S1("James", "Computer Eng.");
+    S1.showStudentInfo();
+    
+    return 0;
+}
